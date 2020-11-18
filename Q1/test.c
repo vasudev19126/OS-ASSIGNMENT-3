d7835d56a0ebf2cb90a820606d40a29aa2d75492
vasudev19126 <vasudev19126@iiitd.ac.in>
2020-11-18 07:45:57 +0530

    final Q3

--- Q1/test.c
+++ Q1/test.c
@@ -8,34 +8,40 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+
 
 int main()
 {
-	long pid,tem,i=0;
+	long pid;
 	pid=fork();
-	if(pid>0)
+	if(pid<0){
+		printf("forked failed");
+		return 0;
+	}
+	else if(pid==0)
 	{
-		
-		printf("%ld  %d\n",pid, (int)getpid());
-		//pid=(int)getpid();
-		syscall(440,12,1000000000);
-		
+		long int sys_return = syscall(440,(int)getpid(),10000000000);
 		struct timeval t1,t2;
 		gettimeofday(&t1,NULL);
+		long i=0;
 		for(i=0;i<10000000000;i++);
-		gettimeofday(&t2,NULL);		
-		printf("\n%lf  this is time 1\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
-		wait(NULL);
+		gettimeofday(&t2,NULL);
+		printf("\n%lf this is time of child\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
+		printf("Syscall returned = %ld \n", sys_return);
+		
 	}
 	else
 	{
+		
 		struct timeval t1,t2;
 		gettimeofday(&t1,NULL);
+		long i=0;
 		for(i=0;i<10000000000;i++);
 		gettimeofday(&t2,NULL);
-		printf("\n%lf  this is time 2\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
+		printf("\n%lf this is time of parent\n",(double)(t2.tv_usec-t1.tv_usec)/1000 + (double)(t2.tv_sec-t1.tv_sec)*1000);
+		wait(NULL);
 		
-
 	}
 	return 0;
 
